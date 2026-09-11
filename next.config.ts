cb664da33d51f3f44e684ee852be73589b39a2f1
mukuhaben <254engineerben@example.com>
2026-09-11 08:55:17 +0300

    configured project to be deployed in github pages for testing by adding github workflows deploy yml file and editing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
+/*
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   config options here 
   reactCompiler: true,
+};
+
+export default nextConfig;
+*/
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+
+  output: "export",
+  basePath: "/Applied-solutions-eXperts",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
